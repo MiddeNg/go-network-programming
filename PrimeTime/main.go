0232178dab60b9655c9d2f4b17a5c848df6db69c
william <william23282328@gmail.com>
2024-11-09 02:06:44 +0800

    correct primetime

--- PrimeTime/main.go
+++ PrimeTime/main.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/MiddeNg/go-network-programming/server"
+	"io"
 	"math"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -18,53 +20,74 @@ type request struct {
 	Number int    `json:"number"`
 }
 
+type request2 struct {
+	Method string  `json:"method"`
+	Number float64 `json:"number"`
+}
+
 func primeTime(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024*1024)
+	buf := make([]byte, 0, 1024*1024)
 	for {
 		var req request
-		n, err := conn.Read(buf)
-		fmt.Println("read", n, "bytes")
-		if err != nil {
-			if err.Error() == "EOF" {
-				return
+		var dummy request2
+		for {
+			n, err := conn.Read(buf[len(buf):cap(buf)])
+			buf = buf[:len(buf)+n]
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				panic(err)
 			}
-			//panic(err)
+			if buf[len(buf)-1] == '\n' {
+				break
+			}
+			if len(buf) == cap(buf) {
+				buf = append(buf, 0)[:n]
+			}
 		}
-		requests := bytes.Split(buf[:n], []byte{'\n'})
+		requests := bytes.Split(buf, []byte{'\n'})
+		buf = buf[:0]
 		for _, request := range requests {
 			if len(request) == 0 {
 				continue
 			}
-			println("request: "+string(request), len(request))
+			//println("request: "+string(request), len(request))
 
 			if err := json.Unmarshal(request, &req); err != nil {
-				fmt.Println("json error: " + err.Error())
+				if err := json.Unmarshal(request, &dummy); err == nil {
+					fmt.Println("received float number" + "  request: " + string(request))
+					conn.Write([]byte("{\"method\":\"isPrime\",\"prime\":false}\n"))
+					continue
+				}
+				fmt.Println("json error: " + err.Error() + " request: " + string(request))
 				conn.Write([]byte(err.Error()))
-				//return
-
+				return
 			}
 			if req.Method != "isPrime" {
-				fmt.Println("Method not found")
+				//fmt.Println("Method not found")
 				conn.Write([]byte("Method not found"))
-				//return
-
+				return
 			}
-
+			if !strings.Contains(string(request), "number") {
+				conn.Write([]byte("no number!"))
+				return
+			}
 			inInt := req.Number
-
-			fmt.Printf("float: %f, int: %d\n", req.Number, inInt)
+			println("correct request: " + string(request))
+			//fmt.Printf("float: %f, int: %d\n", req.Number, inInt)
 			if isPrime(inInt) {
 				//example : {"method":"isPrime","prime":false}
 				res := "{\"method\":\"isPrime\",\"prime\":true}\n"
-				fmt.Printf("Prime! num: %d, res: %s\n", inInt, res)
+				//fmt.Printf("request: "+string(request), len(request))
 				if _, err := conn.Write([]byte(res)); err != nil {
 					fmt.Println(err)
 					//panic(err)
 				}
 			} else {
 				res := "{\"method\":\"isPrime\",\"prime\":false}\n"
-				fmt.Printf("Not Prime! num: %d, res: %s\n", inInt, res)
+				//fmt.Printf("Not Prime! num: %d, res: %s\n", inInt, res)
 				if _, err := conn.Write([]byte(res)); err != nil {
 					//panic(err)
 					fmt.Println(err)
